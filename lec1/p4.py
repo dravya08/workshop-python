d1d52e0a67a80896244bc25601c6bae78dad0c60
Dravya Gohil <dravyagohil@gmail.com>
2019-12-27 20:45:31 +0530

    second

---
+++ lec1/p4.py
@@ -0,0 +1,13 @@
+# wapp to add two integer
+# supplied by the user 
+
+print("enter the first number")
+s1 = input()
+n1 = int(s1)
+
+n2 = int(input("enter the second number"))
+
+res = n1 + n2 
+print(res)
+print("result = ", res)
+print("result = "+ str(res))
---
+++ lec1/p5.py
@@ -0,0 +1,10 @@
+# wapp to read two integers and exchange their values
+
+n1 = int(input("enter first integer ")) 
+n2 = int(input("enter second integer "))
+
+print("b4" , "n1 =", n1 ,"n2 =", n2)
+n1,n2 = n2,n1
+print("after" , "n1 =", n1 ,"n2 =", n2)
+
+
