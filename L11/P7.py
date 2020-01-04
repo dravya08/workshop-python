ea37ca2d383bc06eaac2abfb010408b9826ea3a5
Dravya Gohil <dravyagohil@gmail.com>
2020-01-04 18:55:48 +0530

    p7

--- L11/P7.py
+++ L11/P7.py
@@ -6,11 +6,23 @@ root.title("Even Odd Calci ")
 root.geometry("400x300+500+400")
 def f1():
     try:
-            s1= entNumber.get()
-            n1 = int(s1)
-            msg = ""
-            if n1 % 2 == 0:
-                msg = "Even"
-            else:
-                msg ="Odd"
-            me
+        s1= entNumber.get()
+        n1 = int(s1)
+        msg = ""
+        if n1 % 2 == 0:
+            msg = "Even"
+        else:
+            msg ="Odd"
+        messagebox.showinfo("Jawab", msg)
+    except ValueError:
+        messagebox.showerror("Galat Kiya", 'u need to enter interger only')
+        entNumber.delete(0,END)
+        entNumber.focus()
+
+lblNumber = Label(root,text ="enter number", font=("arial", 18 ,"bold"))
+entNumber = Entry(root,bd = 5, font=("arial", 18 ,"bold"))
+btnFind = Button(root,text ="FInd", font=("arial", 18 ,"bold"),command=f1)
+lblNumber.pack(pady = 10)
+entNumber.pack(pady = 20)
+btnFind.pack(pady =10)
+root.mainloop()
